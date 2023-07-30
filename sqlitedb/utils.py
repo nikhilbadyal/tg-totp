6b2cbe83f89fbe5df90ee0b07fa3d2d114b12d79
Nikhil Badyal <nikhill773384@gmail.com>
2023-07-30 18:54:12 +0530

    🔥 Removed duplicate code

--- sqlitedb/utils.py
+++ sqlitedb/utils.py
@@ -5,8 +5,6 @@ from typing import Any, Dict, TypeVar
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Model, QuerySet
 from loguru import logger
-from telethon import events
-from telethon.tl.types import User
 
 T = TypeVar("T", bound=Model)
 
@@ -17,24 +15,6 @@ class UserStatus(Enum):
     ACTIVE = "active"
     SUSPENDED = "suspended"
     TEMP_BANNED = "temporarily banned"
-
-
-async def get_user(event: events.NewMessage.Event) -> User:
-    """Get the user associated with a message event in Telegram.
-
-    Args:
-        event (events.NewMessage.Event): The message event.
-
-    Returns:
-        User: The User entity associated with the message event.
-    """
-    try:
-        # Get the user entity from the peer ID of the message event, Uses cache
-        user: User = await event.client.get_entity(event.peer_id)
-    except (ValueError, AttributeError):
-        logger.debug("Invalid Peer ID")
-        user = await event.get_sender()
-    return user
 
 
 class ErrorCodes(Enum):
